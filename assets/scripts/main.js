ffa959bd9d615556b443993c66c5ddd6bb513bbb
Valerii Stolbov <stolbov.v@outlook.com>
2024-06-01 14:06:45 +0500

    Updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -108,7 +108,7 @@ window.addEventListener('DOMContentLoaded', () => {
   new Menu('.js-menu');
   animCard.forEach(card => {
     const animation = gsap.to(card, {
-      '--width': '100%',
+      '--width': '0%',
       ease: 'none',
       duration: 0.5,
       paused: true
